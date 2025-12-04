f5c98fcdcddf49f82c3e5452084b935e2c0cdb5e
7003982-lab <7003982@spsd.sk.ca>
2025-12-04 12:19:19 -0600

    Orbit lines

--- Space Sim Capstone/sketch.js
+++ Space Sim Capstone/sketch.js
@@ -5,6 +5,10 @@
 // Global Variables
 let g;
 object = []
+// let cam;
+// let camX = 0;
+// let camY = 0;
+// let camZ = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -15,6 +19,8 @@ function setup() {
   object.push(new Planet(-1140,100, 3.4)); // Mars
   object.push(new Planet(2050,100, 71.5)); // Jupiter
   object.push(new Planet(-3000,100, 60)); // Saturn
+  // cam = createCamera();
+  // setCamera(cam);
 }
 
 function draw() {
@@ -22,9 +28,13 @@ function draw() {
   star(0,0, 349);
   orbitControl()
   for(let o of object){
-    o.display();
+    o.allFunction();
   }
   pointLight(255,190,50, 0, 0, 0);
+  // cam.setPosition(camX,camY,camZ);
+  // if(keyIsDown(RIGHT_ARROW)){
+  //   camX += 100;
+  // }
 }
 
 function star(x, y, d){
@@ -56,4 +66,23 @@ class Planet{
     sphere(this.d)
     pop();
   }
+
+  orbit(){
+    strokeWeight(5);
+    stroke(220);
+    noFill();
+    push();
+    circle(0, 0, (this.x)*2);
+    pop();
+  }
+
+  move(){
+
+  }
+
+  allFunction(){
+    this.display();
+    this.orbit();
+  }
+  
 }
