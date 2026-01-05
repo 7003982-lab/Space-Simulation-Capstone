d2d09a9d0d11114b23afe79c813a835f2e6f6293
7003982-lab <7003982@spsd.sk.ca>
2026-01-05 14:09:38 -0600

    Textures

--- Space Sim Capstone/sketch.js
+++ Space Sim Capstone/sketch.js
@@ -6,52 +6,60 @@
 let G = 6.67e-11;
 let sun;
 let sunT;
+let mercuryT;
+let venusT;
+let earthT;
+let marsT;
+let jupiterT;
 let sunMass = 2e30;
 let planets = [];
-let sunD = 349;
-// let cam;
-// let camX = 0;
-// let camY = 0;
-// let camZ = 0;
+let sunD = 350
+;
 
 
 
 
-function setup() {
-  loadAssets();
+async function setup() {
+  await loadAssets();
+  //frameRate(1);
   createCanvas(windowWidth, windowHeight, WEBGL);
-  angleMode(DEGREES)
-  // planets.push(new Planet(57.9, 0, 2.4, 3.3e23)); // Mercury
-  // planets.push(new Planet(108, 0, 6, 4.9e24)); // Venus
-  // planets.push(new Planet(149, 0, 6.2, 5.97e24)); // Earth
-  // planets.push(new Planet(227, 0, 3.4, 6.4e23)); // Mars
-  planets.push(new Planet(778, 0, 71.5, 1.89e27)); // Jupiter
-  // planets.push(new Planet(1429, 0, 60, 5.67e26)); // Saturn
+  angleMode(DEGREES);
+  planets.push(new Planet(57.9, 0, 2.4, 3.3e23, 20, mercuryT)); // Mercury
+  planets.push(new Planet(108, 0, 6, 4.9e24, 20, venusT)); // Venus
+  planets.push(new Planet(149, 0, 6.3, 5.97e24, 20, earthT)); // Earth
+  planets.push(new Planet(227, 0, 3.4, 6.4e23, 20, marsT)); // Mars
+  planets.push(new Planet(778, 0, 70, 1.89e27, 30, jupiterT)); // Jupiter
+  planets.push(new Planet(1429, 0, 58, 5.67e26, 60, mercuryT)); // Saturn
 }
 
 async function loadAssets(){
-  sunT = await loadImage("assets/sun/sun.jpg");
+  sunT = await loadImage("assets/textures/sun.jpg");
+  mercuryT = await loadImage("assets/textures/mercury.jpg");
+  venusT = await loadImage("assets/textures/venus.jpg");
+  earthT = await loadImage("assets/textures/earth.jpg");
+  marsT = await loadImage("assets/textures/mars.jpg");
+  jupiterT = await loadImage("assets/textures/jupiter.jpg");
 }
 
 function draw() {
   background(0);
   star(0,0, sunD);
   orbitControl();
-  push()
+  push();
   scale(100);
-  //model(sun);
   
-  pop()
+  pop();
   
   for(let o of planets){
     o.allFunction();
   }
+
   stroke("red");
   line(0,0,1000,0);
   stroke("blue");
   line(0,0,0,1000);
   stroke("green");
-  line(0,0,0,0,0,1000)
+  line(0,0,0,0,0,1000);
 }
 
 function star(x, y, d){
@@ -64,14 +72,17 @@ function star(x, y, d){
 }
 
 class Planet{
-  constructor(x,y,d,m){
+  constructor(x,y,d,m,vl,t){
     this.x = x;
     this.y = y;
     this.d = d;
     this.m = m;
+    this.t = t;
+    this.vl = vl;
     this.pos = createVector(this.x+ sunD, this.y);
     this.vel = createVector(0, (sqrt(G*sunMass/(this.x*10e13)))); // ((G*m)/r)^1/2
-    this.gravForce = (((G*(this.m*sunMass))/(this.x*10e11)**2))
+    this.gravForce = (((G*(this.m*sunMass))/((this.x*10e11)**2)))
+    this.vel.limit(this.vl);
   }
 
   calcStar(){
@@ -91,10 +102,13 @@ class Planet{
   }
 
  display(){
-    stroke(150,150,180);
-    fill(160,160,180);
+    //stroke(150,150,180);
+    
     push();
     translate(this.pos.x,this.pos.y,0);
+    noStroke();
+    //fill(0);
+    texture(this.t);
     sphere(this.d);
     //console.log(this.pos.x, this.pos.y)
     pop();
